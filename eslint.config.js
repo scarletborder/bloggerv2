a2b4048280fc353d7d493f2ba6a992f61a151e44
songrujia <songrujia@tencent.com>
2025-11-14 16:19:52 +0800

    feat: 添加代码检查ci

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,21 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { globalIgnores, defineConfig } from 'eslint/config'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import { globalIgnores } from 'eslint/config';
+import tencentEslintConfig from 'eslint-config-tencent/flat';
 
 export default tseslint.config([
   globalIgnores(['dist']),
+  ...tencentEslintConfig({
+    tsconfigRootDir: process.cwd(),
+    project: './tsconfig.json',
+  }),
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
-      tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
     ],
@@ -21,4 +25,4 @@ export default tseslint.config([
     },
   },
   reactHooks.configs.flat.recommended,
-])
+]);
